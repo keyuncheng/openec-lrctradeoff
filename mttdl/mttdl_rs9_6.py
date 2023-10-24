2f7ed590d9d3b05ca0338c105fde4d9970e842a9
Keyun Cheng <keyun_cheng@qq.com>
2023-10-24 09:35:14 +0800

    first commit

---
+++ mttdl/mttdl_rs9_6.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+# RS(16,12) MTTDL
+
+# num_nodes
+num_nodes = 400
+
+# bw (Gbps)
+bw_Gbps = 1
+
+# data (TB)
+data_TB = 16
+
+# mttf_node_year
+mttf_node_year = 4
+
+# repair_time (min)
+repair_time_min = 30
+
+# avail_rp_bw (percentage)
+avail_rp_bw = 0.1
+
+# single repair cost
+single_rp_cost = 6
+
+# single node fail rate
+single_node_fail_rate_year = 1.0 / mttf_node_year
+
+# node repair rate (single, multiple)
+single_rp_rate_year = 1.0 * avail_rp_bw * (num_nodes - 1) * (bw_Gbps * (3600 * 24 * 365)) * (10**9) / (single_rp_cost * data_TB * (10**12) * 8)
+multi_rp_rate_year = 1.0 * (3600 * 24 * 365) / ((30 * 60))
+
+print(single_rp_cost)
+print(single_node_fail_rate_year)
+print(single_rp_rate_year)
+print(multi_rp_rate_year)
+
+lp_mtx = np.array([
+    [-(9*single_node_fail_rate_year), single_rp_rate_year, 0.0, 0.0],
+    [9*single_node_fail_rate_year, -(8*single_node_fail_rate_year+single_rp_rate_year), multi_rp_rate_year, 0.0],
+    [0.0, 8*single_node_fail_rate_year, -(7*single_node_fail_rate_year+multi_rp_rate_year), multi_rp_rate_year],
+    [0.0, 0.0, 7*single_node_fail_rate_year, -(6*single_node_fail_rate_year+multi_rp_rate_year)]
+    ])
+
+right = np.array([-1, 0.0, 0.0, 0.0])
+
+print(lp_mtx)
+print(right)
+sol = np.linalg.solve(lp_mtx, right)
+print(sol)
+print("{:e}".format(np.sum(sol)))
+
+print(np.sum(sol))
