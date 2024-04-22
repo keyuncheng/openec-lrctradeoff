437b869a5194cf74f7e80908a930713de1cb5529
Keyun Cheng <keyun_cheng@qq.com>
2024-04-22 21:56:17 +0800

    add test for AzureLRCTradeoff

--- openec-lrctradeoff/src/ec/ECBase.cc
+++ openec-lrctradeoff/src/ec/ECBase.cc
@@ -1,15 +1,9 @@
 #include "ECBase.hh"
 
-ECBase::ECBase(){
+ECBase::ECBase()
+{
 }
 
-ECBase::ECBase(int n, int k, int w, int opt, vector<string> param) {
-}
-
-vector<int> ECBase::GetNodeSymbols(int nodeid) {
-    return vector<int>();
-}
-
-vector<vector<int>> ECBase::GetLayout() {
-    return vector<vector<int>>();
+ECBase::ECBase(int n, int k, int w, int opt, vector<string> param)
+{
 }
--- openec-lrctradeoff/src/ec/ECBase.hh
+++ openec-lrctradeoff/src/ec/ECBase.hh
@@ -7,38 +7,19 @@
 
 using namespace std;
 
-class ECBase {
-  public:
-    int _n, _k, _w;
-    //bool _locality;
-    int _opt;
+class ECBase
+{
+public:
+  int _n, _k, _w;
+  // bool _locality;
+  int _opt;
 
-    ECBase();
-    ECBase(int n, int k, int w, int opt, vector<string> param);
-    
-    virtual ECDAG* Encode() = 0;
-    virtual ECDAG* Decode(vector<int> from, vector<int> to) = 0;
-    virtual void Place(vector<vector<int>>& group) = 0;
+  ECBase();
+  ECBase(int n, int k, int w, int opt, vector<string> param);
 
-    /**
-     * additional functions
-     * Note: It's impure virtual so as not to implement them for every code
-     */
-
-    /**
-     * @brief Get symbols from a node
-     * 
-     * @param nodeid 
-     * @return vector<int> 
-     */
-    virtual vector<int> GetNodeSymbols(int nodeid);
-    
-    /**
-     * @brief get node layout
-     * 
-     * @return vector<vector<int>> 
-     */
-    virtual vector<vector<int>> GetLayout();
+  virtual ECDAG *Encode() = 0;
+  virtual ECDAG *Decode(vector<int> from, vector<int> to) = 0;
+  virtual void Place(vector<vector<int>> &group) = 0;
 };
 
 #endif
