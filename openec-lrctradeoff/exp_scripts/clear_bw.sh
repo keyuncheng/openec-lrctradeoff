107f50f975df085d75ad98af0a6ac6b208954952
Keyun Cheng <keyun_cheng@qq.com>
2024-05-03 10:39:13 +0800

    update scripts for hierarchical topology

--- openec-lrctradeoff/exp_scripts/clear_bw.sh
+++ openec-lrctradeoff/exp_scripts/clear_bw.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# usage: clear bandwidth setting
+# usage: clear bandwidth setting (on the gateway node)
 
-cur_ip=$(ifconfig | grep '192.168.0' | head -1 | sed "s/ *inet [addr:]*\([^ ]*\).*/\1/")
+passwd=kycheng
+home_dir=$(echo ~)
+wondershaper_dir=$home_dir/wondershaper
+
+cur_ip=$(ifconfig | grep '192.168.10' | head -1 | sed "s/ *inet [addr:]*\([^ ]*\).*/\1/")
 cur_dev=$(ifconfig | grep -B1 $cur_ip | grep -o "^\w*")
 
-tc filter del dev $cur_dev > /dev/null 2>&1
-tc qdisc del dev $cur_dev root > /dev/null 2>&1
+cd $wondershaper_dir && echo $passwd | sudo -S ./wondershaper -c -a $cur_dev
 
-echo disable bandwidth setting [ $cur_ip $cur_dev ]
+echo clear bandwidth setting [ $cur_ip $cur_dev ]
